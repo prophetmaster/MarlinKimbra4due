1df4dba7d5874aa7e4cec37d96f1240ac132724d
Buchs Sullivan <sullivan.buchs@gmail.com>
2015-11-17 22:04:33 +0100

    update

--- MarlinKimbra4due/Configuration_Basic.h
+++ MarlinKimbra4due/Configuration_Basic.h
@@ -194,17 +194,17 @@
 #define BED_MINTEMP      5 // (degC)
 
 //Preheat Constants
-#define PLA_PREHEAT_HOTEND_TEMP 190
+#define PLA_PREHEAT_HOTEND_TEMP 195
 #define PLA_PREHEAT_HPB_TEMP 60
-#define PLA_PREHEAT_FAN_SPEED 255   // Insert Value between 0 and 255
+#define PLA_PREHEAT_FAN_SPEED 0   // Insert Value between 0 and 255
 
-#define ABS_PREHEAT_HOTEND_TEMP 240
+#define ABS_PREHEAT_HOTEND_TEMP 235
 #define ABS_PREHEAT_HPB_TEMP 100
-#define ABS_PREHEAT_FAN_SPEED 255   // Insert Value between 0 and 255
+#define ABS_PREHEAT_FAN_SPEED 0   // Insert Value between 0 and 255
 
-#define GUM_PREHEAT_HOTEND_TEMP 230
+#define GUM_PREHEAT_HOTEND_TEMP 235
 #define GUM_PREHEAT_HPB_TEMP 60
-#define GUM_PREHEAT_FAN_SPEED 255   // Insert Value between 0 and 255
+#define GUM_PREHEAT_FAN_SPEED 0   // Insert Value between 0 and 255
 /*****************************************************************************************************/
 
 
